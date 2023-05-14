8fff5f6d0ed19fef5815075e302c085872dab5df
Pengju Zhang <xenthorpis2016@gmail.com>
2023-05-14 00:00:08 -0400

    init project

---
+++ mian.py
@@ -0,0 +1,5 @@
+from module import *
+
+if __name__ == "__main__":
+    fr = face_recognition.FaceRecognition()
+    fr.print_hello_world()
--- module/__init__.py
+++ module/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __author__ = """Pengju Zhang"""
-__email__ = 'inupedia.official@gmail.com'
-__version__ = '0.0.1'
+__email__ = "inupedia.official@gmail.com"
+__version__ = "0.0.1"
+
+from .face_recognition import FaceRecognition
