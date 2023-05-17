437a84d2bf5efa9fceb439835b691c339a520eb2
Pengju Zhang <xenthorpis2016@gmail.com>
2023-05-17 16:32:10 -0400

    add deepface

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ import asyncio
 
 if __name__ == "__main__":
     # stream from Bilibili
-    bilibili_stream = BilibiliStream("4926600")
+    # 4926600 26098238 22947919
+    bilibili_stream = BilibiliStream("24489698")
     asyncio.run(bilibili_stream.start())
